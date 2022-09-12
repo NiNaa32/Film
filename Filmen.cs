57769f3e9d9b5934e313a47f8e0121eb2f38c786
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-09-12 18:47:30 +0200

    Add project files.

---
+++ Filmen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Film
+{
+    class Filmen
+    {
+        private string titel;
+        private double kostnader;
+        private string skådespelare;
+        private double sponsring;
+
+        public string GetSetTitel
+        {
+            set { titel = value; }
+            get { return titel; }
+        }
+
+        public double GetSetKostnader
+        {
+            set { kostnader = value; }
+            get { return kostnader; }
+        }
+
+        public string GetSetSkådespelare
+        {
+            set { skådespelare = value; }
+            get { return skådespelare; }
+        }
+
+        public double GetSetSponsring
+        {
+            set { sponsring = value; }
+            get { return sponsring; }
+        }
+
+        public Filmen()
+        {
+            titel = "";
+            skådespelare = "";
+            kostnader = 0.0;
+            sponsring = 0.0;
+        }
+
+        public Filmen(string t, string s, double k, double sp)
+        {
+            titel = t;
+            skådespelare = s;
+            kostnader = k;
+            sponsring = sp;
+        }
+        public double Beräkna(Filmen f, int antTittare, double biljettpris)
+        {
+            double feeFilm;
+            feeFilm = (double)biljettpris * antTittare + f.GetSetSponsring - f.GetSetKostnader;
+            return feeFilm;
+        }
+
+        public double FeeS(double feeFilm)
+        {
+            double feeS;
+            feeS = feeFilm * 0.05;
+            return feeS;
+        }
+        static void Registrering(Filmen f)
+        {
+            Console.WriteLine("Filmtitel: ");
+            f.GetSetTitel = Console.ReadLine();
+            Console.WriteLine("Filmkostnad: ");
+            f.GetSetKostnader = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Huvudskådespelarens namn: ");
+            f.GetSetSkådespelare = Console.ReadLine();
+            Console.WriteLine("Sponsorskap: ");
+            f.GetSetSponsring = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Film;
+
+static void Registrering(Filmen f)
+{
+    Console.WriteLine("Filmtitel: ");
+    f.GetSetTitel = Console.ReadLine();
+    Console.WriteLine("Filmkostnad: ");
+    f.GetSetKostnader = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Huvudskådespelarens namn: ");
+    f.GetSetSkådespelare = Console.ReadLine();
+    Console.WriteLine("Sponsorskap: ");
+    f.GetSetSponsring = Convert.ToDouble(Console.ReadLine());
+}
+
+int n, i, antTittare;
+double biljettpris;
+Console.WriteLine("Ange antalet filmer");
+n = Convert.ToInt32(Console.ReadLine());
+Filmen[] f = new Filmen[n];
+double[] feeS = new double[n];
+double[] feeFilm = new double[n];
+for (i = 0; i < n; i++)
+{
+    f[i] = new Filmen();
+    Console.WriteLine("Ange information om {0}.filmen", i);
+    Registrering(f[i]);
+    Console.WriteLine("Ange antalet tittare");
+    antTittare = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ange biljettpriset");
+    biljettpris = Convert.ToDouble(Console.ReadLine());
+    feeFilm[i] = f[i].Beräkna(f[i], antTittare, biljettpris);
+    feeS[i] = f[i].FeeS(feeFilm[i]);
+    Console.WriteLine("*************************************************");
+}
+Console.WriteLine();
+Console.WriteLine("****************************");
+for (i = 0; i < n; i++)
+    Console.WriteLine("Filmen {0} har uppnått en avgift av  {1} kronor, och huvudskådespelaren {2} intjänade  {3} kronor.", f[i].GetSetTitel, feeFilm[i], f[i].GetSetSkådespelare, feeS[i]);
+Console.ReadLine();
+
